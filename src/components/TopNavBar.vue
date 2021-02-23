201b883631c28386b1bdd25987d0cf023caafe2b
Cícero Neto <cicero.alex@gmail.com>
2021-02-23 14:50:36 -0300

    Changed TopNavBar

--- src/components/TopNavBar.vue
+++ src/components/TopNavBar.vue
@@ -4,25 +4,44 @@
       <b-navbar-brand href="#">Vue JsSIP</b-navbar-brand>
       <b-navbar-nav class="ml-auto">
         <!-- Navbar dropdowns -->
-        <b-nav-item-dropdown text="Lang" right>
-          <b-dropdown-item href="#">EN</b-dropdown-item>
-          <b-dropdown-item href="#">ES</b-dropdown-item>
-          <b-dropdown-item href="#">RU</b-dropdown-item>
-          <b-dropdown-item href="#">FA</b-dropdown-item>
-        </b-nav-item-dropdown>
-
-        <b-nav-item-dropdown text="User" right>
-          <b-dropdown-item href="#">Account</b-dropdown-item>
-          <b-dropdown-item href="#">Settings</b-dropdown-item>
-        </b-nav-item-dropdown>
+        <b-dropdown id="dropdown-1" right>
+          <template #button-content>
+            <i class="fa fa-phone"></i>
+          </template>
+          <div class="p-2 pt-0 notification-body">
+            <strong>Ligações</strong>
+            <b-dropdown-divider></b-dropdown-divider>
+            <b-dropdown-item
+              v-for="(item, index) in notifications"
+              :key="index"
+            >
+              <b-container class="bv-example-row">
+                <b-row>
+                  <b-col cols="10"
+                    ><span>{{ item.name }}</span>
+                    <br />
+                    <small>{{ item.sip }}</small></b-col
+                  >
+                  <b-col cols="2"><i class="fa fa-phone"></i></b-col>
+                </b-row>
+              </b-container>
+            </b-dropdown-item>
+          </div>
+        </b-dropdown>
       </b-navbar-nav>
     </b-navbar>
   </div>
 </template>
 
 <script>
+import notifications from "../data/notifications";
 export default {
   name: "TopNavBar",
+  data() {
+    return {
+      notifications,
+    };
+  },
 };
 </script>
 
---
+++ src/data/notifications.js
@@ -0,0 +1,23 @@
+const data = [
+  {
+    name: 'Joisse Kaycee just sent a new comment!',
+    sip: '6002',
+    id: 1
+  },
+  {
+    name: '1 item is out of stock!',
+    sip: '6003',
+    id: 2
+  },
+  {
+    name: 'New order received! It is total $147,20.',
+    sip: 'sip:teste_cbrdxv@tryit.jssip.net',
+    id: 3
+  },
+  {
+    name: '3 items just added to wish list by a user!',
+    sip: 'sip:casn_cbrdxv@tryit.jssip.net',
+    id: 4
+  }
+]
+export default data
